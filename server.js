8046fa8b4220102eb21ea731efd11be1a95aac63
wcipriano <wagner@solucionarti.com.br>
2023-11-01 18:33:47 -0300

    :construction: add route "/api/produtos" to static json data products: step 2.1

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 
+const apiRouter = require("./api/routes/apiRouter");
+app.use("/api", apiRouter);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
